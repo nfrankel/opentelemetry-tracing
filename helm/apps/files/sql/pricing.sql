e98df706a361e38031f3a3e4dafa355fc1f17ff4
Nicolas Fränkel <nicolas@frankel.ch>
2022-08-12 15:05:39 +0200

    Initial commit

---
+++ helm/apps/files/sql/pricing.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA pricing;
+CREATE TABLE pricing.price
+(
+    id     BIGINT PRIMARY KEY,
+    value  FLOAT NOT NULL,
+    jitter FLOAT NOT NULL
+);
+INSERT INTO pricing.price(id, value, jitter)
+VALUES (1, 0.49, 0.1),
+       (2, 1.49, 0.1),
+       (3, 9.49, 0.3),
+       (4, 49.49, 0.2);
