f526f45cd5c36b1b9f39f3887030f17289ab153f
Nicolas Fränkel <nicolas@frankel.ch>
2022-08-12 15:05:39 +0200

    Initial commit

---
+++ stock/src/data.rs
@@ -0,0 +1,104 @@
+use crate::Warehouse;
+
+pub(crate) static WAREHOUSES: [Warehouse; 20] = [
+    Warehouse {
+        id: 1,
+        city: "Popejoy",
+        state: "Iowa",
+    },
+    Warehouse {
+        id: 2,
+        city: "Hooker",
+        state: "Oklahoma",
+    },
+    Warehouse {
+        id: 3,
+        city: "China",
+        state: "Texas",
+    },
+    Warehouse {
+        id: 4,
+        city: "Rainbow City",
+        state: "Alabama",
+    },
+    Warehouse {
+        id: 5,
+        city: "Blue Grass",
+        state: "Iowa",
+    },
+    Warehouse {
+        id: 6,
+        city: "Pink",
+        state: "Oklahoma",
+    },
+    Warehouse {
+        id: 7,
+        city: "Colon",
+        state: "Michigan",
+    },
+    Warehouse {
+        id: 8,
+        city: "Cool",
+        state: "Texas",
+    },
+    Warehouse {
+        id: 9,
+        city: "Oblong",
+        state: "Illinois",
+    },
+    Warehouse {
+        id: 10,
+        city: "Speed",
+        state: "North Carolina",
+    },
+    Warehouse {
+        id: 11,
+        city: "Last Chance",
+        state: "Iowa",
+    },
+    Warehouse {
+        id: 12,
+        city: "Uncertain",
+        state: "Texas",
+    },
+    Warehouse {
+        id: 13,
+        city: "Winnebago",
+        state: "Minnesota",
+    },
+    Warehouse {
+        id: 14,
+        city: "Climax",
+        state: "Michigan",
+    },
+    Warehouse {
+        id: 15,
+        city: "Three Way",
+        state: "Tennessee",
+    },
+    Warehouse {
+        id: 16,
+        city: "Coward",
+        state: "South Carolina",
+    },
+    Warehouse {
+        id: 17,
+        city: "Okay",
+        state: "Oklahoma",
+    },
+    Warehouse {
+        id: 18,
+        city: "Atomic City",
+        state: "Idaho",
+    },
+    Warehouse {
+        id: 19,
+        city: "Superior",
+        state: "Wyoming",
+    },
+    Warehouse {
+        id: 20,
+        city: "Canadian",
+        state: "Texas",
+    },
+];
