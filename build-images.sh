44f343eeeeaeea37f6071183bf23f05b57a65c81
Nicolas Fränkel <nicolas@frankel.ch>
2022-08-12 15:05:39 +0200

    Initial commit

---
+++ build-images.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export PROJECT_HOME=`pwd`
+
+git checkout master~2
+cd $PROJECT_HOME/catalog
+docker build -t otel-catalog:1.0 .
+cd $PROJECT_HOME/pricing
+docker build -t otel-pricing:1.0 .
+cd $PROJECT_HOME/inventory
+docker build -t otel-inventory:1.0 .
+cd $PROJECT_HOME/warehouse-us
+docker build -t otel-warehouse-us:1.0 .
+cd $PROJECT_HOME/warehouse-eu
+docker build -t otel-warehouse-eu:1.0 .
+cd $PROJECT_HOME/warehouse-jp
+docker build -t otel-warehouse-jp:1.0 .
+cd $PROJECT_HOME/warehouse-uk
+docker build -t otel-warehouse-uk:1.0 .
+
+git checkout master~1
+cd $PROJECT_HOME/catalog
+docker build -t otel-catalog:1.1 .
+cd $PROJECT_HOME/pricing
+docker build -t otel-pricing:1.1 .
+
+git checkout master
+cd $PROJECT_HOME/catalog
+docker build -t otel-catalog:1.2 .
+cd $PROJECT_HOME/pricing
+docker build -t otel-pricing:1.2 .
+cd $PROJECT_HOME/analytics
+docker build -t otel-analytics:1.0 .
+
+cd $PROJECT_HOME
