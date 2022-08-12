4d8f527e019866db9865f99f19d39516266efe78
Nicolas Fränkel <nicolas@frankel.ch>
2022-08-12 15:05:39 +0200

    Initial commit

---
+++ catalog/src/main/kotlin/Model.kt
@@ -0,0 +1,18 @@
+package ch.frankel.catalog
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.springframework.data.annotation.Id
+
+data class Product(@Id val id: Long, val name: String, val description: String)
+
+data class Price(val price: Float)
+
+data class InStockLevel(@JsonIgnore val productId: Int, val quantity: Int, val warehouse: InWarehouse)
+
+data class InWarehouse(@JsonIgnore val id: Int, val city: String, val state: String)
+
+data class OutStockLevel(val quantity: Int, val warehouse: OutWarehouse)
+
+data class OutWarehouse(val city: String, val state: String)
+
+data class ProductWithDetails(val name: String, val description: String, val price: Float, val stocks: Array<OutStockLevel>)
