b448745e6646d19ac9774f4bfd82b06cd25ddefa
Nicolas Fränkel <nicolas@frankel.ch>
2022-08-12 15:05:39 +0200

    Initial commit

---
+++ warehouse-eu/src/main/kotlin/WarehouseApplication.kt
@@ -0,0 +1,26 @@
+package ch.frankel.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+
+
+@RestController
+class StockLevelController(private val repository: StockLevelRepository) {
+
+    @GetMapping("/stocks")
+    fun stockLevels(): Flux<StockLevel> = repository.findAll()
+
+    @GetMapping("/stocks/{id}")
+    suspend fun stockLevel(@PathVariable("id") id: Long) = repository.findByIdWithWarehouse(id)
+}
+
+@SpringBootApplication
+class WarehouseApplication
+
+fun main(args: Array<String>) {
+    runApplication<WarehouseApplication>(*args)
+}
